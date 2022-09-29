6f51e738e86451a0825c4a59b178fe1607c6d16f
Namelesd <rodalvaram@gmail.com>
2022-09-28 22:43:55 -0500

    Agregando todos los archivos

---
+++ listadepalabras.js
@@ -0,0 +1,6 @@
+var totalPalabras = ["PODEROSO","ZEUS","CAOS"];
+
+function importarPalabras(){
+    var totalPalabras2 = toString(totalPalabras);
+    localStorage.setItem("listaPalabras",totalPalabras2)
+}
