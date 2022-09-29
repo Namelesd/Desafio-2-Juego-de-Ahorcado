0895543302f6def183f555b4649b6739fc6ad583
Namelesd <rodalvaram@gmail.com>
2022-09-28 22:50:09 -0500

    Quitando archivos que no sirven

--- listadepalabras.js
+++
@@ -1,6 +0,0 @@
-var totalPalabras = ["PODEROSO","ZEUS","CAOS"];
-
-function importarPalabras(){
-    var totalPalabras2 = toString(totalPalabras);
-    localStorage.setItem("listaPalabras",totalPalabras2)
-}
